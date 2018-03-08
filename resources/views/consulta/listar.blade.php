f369fcad6c611c8fc4a3356eee9c71bb462959f9
Euak <ueak21@gmail.com>
2018-03-08 14:01:29 -0300

    exibir json

--- resources/views/consulta/listar.blade.php
+++ resources/views/consulta/listar.blade.php
@@ -1,6 +1,9 @@
 @extends('app')
 
 @section('content')
+<script>
+	var objs = [];
+</script>
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
@@ -12,11 +15,17 @@
 			@if (count($consultas) > 0)
 				@foreach ($consultas as $consulta)
 				<div class="row">
-					<div class="col-md-5">
+					<div class="col-md-4">
 				    	<p>Usuário: {{ $consulta->id_usuario }}</p>
 				    </div>
-					<div class="col-md-5">
+					<div class="col-md-4">
 					    <p>CNPJ: {{ $consulta->cnpj }}</p>
+					</div>
+					<div class="col-md-2">
+					    <input type="button" onclick="exibe({{ $consulta->id }})" value="Exibir">
+					    <script>
+					    	objs[{{ $consulta->id }}] = JSON.parse('<?php echo $consulta->json?>');
+					    </script>
 					</div>
 					<div class="col-md-2">
 					    <a href="{{ url('/excluir') }}/{{ $consulta->id }}">Excluir</a>
@@ -32,6 +41,42 @@
 			@endif
 		</div>
 	</div>
+
+
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					Empresa
+				</div>
+				<div class="col-md-8 col-md-offset-2" id="ajaxResponse">
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
+<script>
+		function exibe(id){
+         	$("#ajaxResponse").html("");
+
+			jQuery.each(objs[id], function(key, val) {
+		                	//console.log(key + ':' +val);
+		                	 var item = $("<div class='row'></div>");
+		                	 var property = $("<div class='col-md-12'>\n"+
+												"<div class='row'>\n"+
+													"<div class='col-md-6'>\n"+
+													key+"\n"+
+													"</div>\n"+
+													"<div class='col-md-6'>\n"+
+													val.trim()+"\n"+
+													"</div>\n"+
+												"</div>\n"+
+											"</div>");
+		                	 item.append(property);
+						  $("#ajaxResponse").append(item);
+						});
+		}
+		location.href = "#ajaxResponse";
+</script>
 @endsection
